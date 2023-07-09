8ab351ea9c9073c5d52c7271f8ec472359049999
longminhdo <longminhdo2000@gmail.com>
2023-07-09 18:33:03 +0700

    feat: done quiz router

---
+++ server/router/quiz.router.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const quiz = require('../controller/quiz.controller');
+const catchAsync = require('../helper/catchAsync');
+const validateAuthentication = require('../middleware/validateAuthentication');
+const { validateQuizId } = require('../middleware/validateId');
+
+const router = express.Router();
+
+router.use(validateAuthentication);
+
+// POST /quiz
+router.post(
+  '/',
+  catchAsync(quiz.createQuiz),
+);
+
+// PUT /quizzes/:quizId
+router.put(
+  '/:quizId',
+  validateQuizId,
+  catchAsync(quiz.updateQuiz),
+);
+
+// GET /quizzes
+router.get('/', catchAsync(quiz.getQuizzes));
+
+// GET /quizzes/:quizId
+router.get('/:quizId',
+  validateQuizId,
+  catchAsync(quiz.getQuizById),
+);
+
+// DELETE /quizzes/:quizId
+router.delete(
+  '/:quizId',
+  validateQuizId,
+  catchAsync(quiz.deleteQuiz),
+);
+
+module.exports = router;
