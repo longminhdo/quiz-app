921a60d6b13a1f57d01a7a2b0eb8648d1c343d1e
longminhdo <longminhdo2000@gmail.com>
2023-07-25 22:18:36 +0700

    Update staging env

--- client/src/configuration/staging.ts
+++ client/src/configuration/staging.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   BE_BASE_URL: 'https://quiz-api.themintiest.com',
-  FE_BASE_URL: 'http://forms.bachkhoa.ai',
+  FE_BASE_URL: 'https://quiz-app-six-omega.vercel.app',
   SSO_DOMAIN: 'https://sso-dot-hust-edu.appspot.com',
   SSO_TEST_DOMAIN: 'https://sso-dot-set-hust.appspot.com',
   ENV: 'prod',
